ee3f752aa0a978215af93ecfa4fd14eb159fd18a
MeiK <meik2333@gmail.com>
2019-09-02 17:00:07 +0800

    Update

--- src/client.rs
+++ src/client.rs
@@ -1,22 +1,20 @@
+use std::io::{self, BufRead};
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket};
 
-fn send(addr: SocketAddr, src: SocketAddr) -> std::io::Result<()> {
-    {
-        let socket = UdpSocket::bind(&addr)?;
-        let mut buf = [0; 10];
-        buf[0] = 1;
-        buf[1] = 2;
-        buf[2] = 3;
-        buf[3] = 4;
-        buf[4] = 5;
-        buf[9] = 9;
-        socket.send_to(&buf, &src)?;
-    }
+fn send(message: String, addr: SocketAddr, src: SocketAddr) -> std::io::Result<()> {
+    let socket = UdpSocket::bind(&addr)?;
+    let buf = message.as_bytes();
+    socket.send_to(&buf, &src)?;
+
     Ok(())
 }
 
 fn main() {
     let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 0));
     let src = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 53));
-    let _ = send(addr, src);
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let message = line.unwrap();
+        let _ = send(message, addr, src).unwrap();
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,15 @@
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket};
 
 fn recv(addr: SocketAddr) -> std::io::Result<()> {
-    {
-        let socket = UdpSocket::bind(&addr)?;
-        let mut buf = [0; 10];
-        let (amt, src) = socket.recv_from(&mut buf)?;
-        println!("{} {}", amt, src);
-        for i in buf.iter() {
-            println!("{}", i);
-        }
-    }
+    let socket = UdpSocket::bind(&addr)?;
+    let mut buf = [0; 512];
+    let (amt, src) = socket.recv_from(&mut buf)?;
+    println!("ip: {}, len: {}", src, amt);
+    let mut message = vec![];
+    message.extend_from_slice(&buf);
+    let message = String::from_utf8(message).unwrap();
+    println!("{}", message);
+
     Ok(())
 }
 
